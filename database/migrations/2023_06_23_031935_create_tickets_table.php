fab07ca24c595fb9714b799bf2f4f3b20e923c61
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2023-06-23 11:44:09 +0700

    model final 1

---
+++ database/migrations/2023_06_23_031935_create_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->decimal('price',10,2);
+            $table->string('ticket_type');
+            $table->unsignedInteger('remain');
+            $table->unsignedInteger('min_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
