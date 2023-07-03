63fcd35b392079bc8d812bc5a1761c8a59fe3ae1
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2023-07-03 17:28:01 +0700

    add modal

--- resources/views/checkout-success.blade.php
+++ resources/views/checkout-success.blade.php
@@ -9,14 +9,60 @@
             z-index: 1000;
             left: 0;
         }
+
+        .contact-form-item {
+            background-color: #fff6d4;
+            border-radius: 10px;
+            border: 3px dashed #ffb489;
+            min-height: 400px;
+            padding: 10px;
+        }
     </style>
     <p class="page-title tilte-custom">Thanh toán thành công</p>
     <img src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688357585/Alvin_Arnold_Votay1_1_jxd9rr.svg"
         id="bfp-context">
-
+    <div class="d-flex justify-content-center align-items-center content-container mt-5">
+        <div class="row w-75 mt-5">
+            <div class="col-12 form-item">
+                <div class="contact-form-item row m-auto">
+                    @for ($i = 1; $i <= ((int) $data['quantity'] > 4 ? 4 : (int) $data['quantity']); $i++)
+                        <div class='col-3'>
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    {!! QrCode::generate($data['string_to_qr']) !!}
+                                    <p class="fw-bold fs-4 mt-2">{{ $data['string_to_qr'] }}</p>
+                                    <p class="fw-bold fs-5">{{ $data['ticket_name'] }}</p>
+                                    <p>---</p>
+                                    <p class="fs-6">Ngày sử dụng: {{ $data['date_order'] }}</p>
+                                    <p>✅</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endfor
+                </div>
+            </div>
+            <p class="mt-3">Số lượng vé: {{ $data['quantity'] }}</p>
+            <form action="{{ route('save') }}" method="post">
+                @csrf
+                <input type="hidden" name="session_id" value="{{ $_GET['session_id'] }}">
+                <input type="hidden" name="string_to_qr" value="{{ $data['string_to_qr'] }}">
+                <div class="d-flex align-items-center justify-content-center">
+                    <div class="w-25 row">
+                        <button class="btn btn-primary col me-1" name="mail" type="submit">Gửi mail</button>
+                        <button class="btn btn-primary col" name="save" type="submit">Tải vé</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        {{-- 
     <div class="row infomation-form-container infomation-form-container1">
-        <div id="index-content">
-            <div class="row form-item">
+        <div class="form-item p-3">
+            <div class="row"
+                style="background-color: #fff6d4;
+            border-radius: 10px;
+            border: 3px dashed #ffb489;
+            min-height: 400px;
+            padding: 10px;">
                 @for ($i = 1; $i <= ((int) $data['quantity'] > 4 ? 4 : (int) $data['quantity']); $i++)
                     <div class='col-3'>
                         <div class="card">
@@ -34,7 +80,7 @@
             </div>
 
         </div>
-        <p>Số lượng vé: {{ $data['quantity'] }}</p>
+        <p class="mt-3">Số lượng vé: {{ $data['quantity'] }}</p>
         <form action="{{ route('save') }}" method="post">
             @csrf
             <input type="hidden" name="session_id" value="{{ $_GET['session_id'] }}">
@@ -46,6 +92,6 @@
                 </div>
             </div>
         </form>
-    </div>
+    </div> --}}
 
-@endsection
+    @endsection
--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -1,94 +1,118 @@
-@extends('layouts.master')
-@section('title', 'Liên hệ')
-@section('content')
-    <style>
-        .contact-info-item {
-            background-color: #fff6d4;
-            border-radius: 10px;
-            border: 3px dashed #ffb489;
-            min-height: 100px;
-            padding: 10px;
-        }
+ <!-- Modal -->
+ <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+     <div class="modal-dialog modal-dialog-centered">
+         <div class="modal-content">
+             <div class="modal-header">
+                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+             </div>
+             <div class="modal-body">
+                 Gửi liên hệ thành công.
+                 <br>
+                 Vui lòng kiên nhẫn đợi phản hồi từ chúng tôi, bạn nhé!
+             </div>
+         </div>
+     </div>
+ </div>
+ @extends('layouts.master')
+ @section('title', 'Liên hệ')
+ @section('content')
+     <style>
+         .contact-info-item {
+             background-color: #fff6d4;
+             border-radius: 10px;
+             border: 3px dashed #ffb489;
+             min-height: 100px;
+             padding: 10px;
+         }
 
-        .contact-form-item {
-            background-color: #fff6d4;
-            border-radius: 10px;
-            border: 3px dashed #ffb489;
-            min-height: 400px;
-            padding: 10px;
-        }
-    </style>
-    <img src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688372713/Alex_AR_Lay_Do_shadow_1_fv9ypm.svg"
-        style="position: absolute; bottom: 20%; left:0; z-index: 100; width: 12%">
-    <p class="page-title tilte-custom">Liên hệ</p>
-    <div class="d-flex justify-content-center align-items-center content-container">
-        <div class="row w-75 mt-5">
-            <div class="row">
-                <div class="col-7 form-item">
-                    <form action="#" class="contact-form-item">
-                        <label class="form-label">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nam non
-                            veritatis illo laborum</label>
-                        <div class="mb-3 row">
-                            <div class="col-4"><input type="text" class="form-control" placeholder="Tên"></div>
-                            <div class="col-8"><input type="text" class="form-control" placeholder="Email"></div>
-                        </div>
-                        <div class="mb-3 row">
-                            <div class="col-4"><input type="text" class="form-control" placeholder="Số điện thoại">
-                            </div>
-                            <div class="col-8"><input type="text" class="form-control" placeholder="Địa chỉ"></div>
-                        </div>
-                        <div class="mb-3 row">
-                            <div class="col-12">
-                                <textarea name="" class="form-control" placeholder="Nhập lời nhắn" rows="4"></textarea>
-                            </div>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center w-50 m-auto">
-                            <button class="btn btn-primary mt-3" type="submit">Gửi liên hệ</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-1"></div>
-                <div class="col-4 flex-column d-flex justify-content-between">
-                    <div class="row mb-4 infomation-item">
-                        <div class="contact-info-item row m-auto">
-                            <div class="col-3">
-                                <img
-                                    src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688373490/adress_1_f196la.svg">
-                            </div>
-                            <div class="col-9">
-                                <p class="mb-0 fw-bold fs-5">Địa chỉ:</p>
-                                <p class="fs-6">86/33 Âu Cơ, Phường 9, Quận Tân Bình, TP. Hồ Chí Minh</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row mb-4 infomation-item">
-                        <div class="contact-info-item row m-auto">
-                            <div class="col-3">
-                                <img
-                                    src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688373490/mail-inbox-app_1_d2mxi0.svg">
-                            </div>
+         .contact-form-item {
+             background-color: #fff6d4;
+             border-radius: 10px;
+             border: 3px dashed #ffb489;
+             min-height: 400px;
+             padding: 10px;
+         }
+     </style>
 
-                            <div class="col-9">
-                                <p class="mb-0 fw-bold fs-5">Email:</p>
-                                <p class="fs-6">investigate@your-site.com</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row mb-3 infomation-item">
-                        <div class="contact-info-item row m-auto">
-                            <div class="col-3">
-                                <img
-                                    src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688373490/telephone_2_quuhc9.svg">
-                            </div>
-                            <div class="col-9">
-                                <p class="mb-0 fw-bold fs-5">Điện thoại:</p>
-                                <p class="fs-6">+84 145 689 798</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+     <img src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688372713/Alex_AR_Lay_Do_shadow_1_fv9ypm.svg"
+         style="position: absolute; bottom: 20%; left:0; z-index: 100; width: 12%">
+     <p class="page-title tilte-custom">Liên hệ</p>
+     <div class="d-flex justify-content-center align-items-center content-container">
+         <div class="row w-75 mt-5">
+             <div class="row">
+                 <div class="col-7 form-item">
+                     <form action="#" class="contact-form-item">
+                         <label class="form-label">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nam
+                             non
+                             veritatis illo laborum</label>
+                         <div class="mb-3 row">
+                             <div class="col-4"><input type="text" class="form-control" placeholder="Tên"></div>
+                             <div class="col-8"><input type="text" class="form-control" placeholder="Email"></div>
+                         </div>
+                         <div class="mb-3 row">
+                             <div class="col-4"><input type="text" class="form-control" placeholder="Số điện thoại">
+                             </div>
+                             <div class="col-8"><input type="text" class="form-control" placeholder="Địa chỉ"></div>
+                         </div>
+                         <div class="mb-3 row">
+                             <div class="col-12">
+                                 <textarea name="" class="form-control" placeholder="Nhập lời nhắn" rows="4"></textarea>
+                             </div>
+                         </div>
+                         <div class="d-flex align-items-center justify-content-center w-50 m-auto">
+                             {{-- <button class="btn btn-primary mt-3" type="submit">Gửi liên hệ</button> --}}
+                             <!-- Button trigger modal -->
+                             <button type="button" class="btn btn-primary mt-3" data-bs-toggle="modal"
+                                 data-bs-target="#exampleModal">
+                                 Gửi liên hệ
+                             </button>
+                         </div>
+                     </form>
+                 </div>
+                 <div class="col-1"></div>
+                 <div class="col-4 flex-column d-flex justify-content-between">
+                     <div class="row mb-4 infomation-item">
+                         <div class="contact-info-item row m-auto">
+                             <div class="col-3">
+                                 <img
+                                     src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688373490/adress_1_f196la.svg">
+                             </div>
+                             <div class="col-9">
+                                 <p class="mb-0 fw-bold fs-5">Địa chỉ:</p>
+                                 <p class="fs-6">86/33 Âu Cơ, Phường 9, Quận Tân Bình, TP. Hồ Chí Minh</p>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="row mb-4 infomation-item">
+                         <div class="contact-info-item row m-auto">
+                             <div class="col-3">
+                                 <img
+                                     src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688373490/mail-inbox-app_1_d2mxi0.svg">
+                             </div>
 
-        </div>
-    </div>
-@endsection
+                             <div class="col-9">
+                                 <p class="mb-0 fw-bold fs-5">Email:</p>
+                                 <p class="fs-6">investigate@your-site.com</p>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="row mb-3 infomation-item">
+                         <div class="contact-info-item row m-auto">
+                             <div class="col-3">
+                                 <img
+                                     src="https://res.cloudinary.com/dpobeimdp/image/upload/v1688373490/telephone_2_quuhc9.svg">
+                             </div>
+                             <div class="col-9">
+                                 <p class="mb-0 fw-bold fs-5">Điện thoại:</p>
+                                 <p class="fs-6">+84 145 689 798</p>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+
+         </div>
+     </div>
+
+
+ @endsection
