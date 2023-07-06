8f716a85ea85a1fd0c7acc85980359ec00730c95
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2023-07-06 17:44:04 +0700

    add keep valid value

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,9 @@
         <div class="col-8 infomation-item">
             <div id="index-content">
                 <p>Chào mừng đến với Little And Little</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia necessitatibus doloribus placeat, ab voluptates laborum sequi consectetur accusamus explicabo commodi numquam magni quo praesentium tempora assumenda deserunt totam consequatur.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia necessitatibus doloribus placeat,
+                    ab voluptates laborum sequi consectetur accusamus explicabo commodi numquam magni quo praesentium
+                    tempora assumenda deserunt totam consequatur.</p>
 
                 <ul>
                     <li>Số vé phải nhiều hơn 1</li>
@@ -32,7 +34,7 @@
                 <div class="row">
                     <div class="col-5">
                         <input class="form-control mt-3 pe-0" placeholder="Số lượng vé" type="number" name="quantity"
-                            id="quantity" required>
+                            id="quantity" required value="{{ old('quantity') }}">
                         @error('quantity')
                             <em class="text-danger fs-6">
                                 {{ $message }}
@@ -41,7 +43,7 @@
                     </div>
                     <div class="col-7 ps-0">
                         <input class="form-control mt-3" placeholder="Ngày đặt vé" type="date" name="date_order"
-                            id="date_order" required>
+                            id="date_order" value="{{ old('date_order') }}" required>
                         @error('date_order')
                             <em class="text-danger fs-6">
                                 {{ $message }}
@@ -50,15 +52,16 @@
                     </div>
                 </div>
                 <input class="form-control mt-3" placeholder="Họ và tên" type="text" name="name" id="name"
-                    required>
+                    required value="{{ old('name') }}">
                 <input class="form-control mt-3" placeholder="Số điện thoại" type="text" name="phone" id="phone"
-                    required>
+                    value="{{ old('phone') }}" required>
                 @error('phone')
                     <em class="text-danger fs-6">
                         {{ $message }}
                     </em>
                 @enderror
-                <input class="form-control mt-3" placeholder="Email" type="email" name="email" id="email" required>
+                <input class="form-control mt-3" placeholder="Email" type="email" name="email" id="email" required
+                    value="{{ old('email') }}">
                 <div class="d-flex align-items-center justify-content-center">
                     <button class="btn btn-primary mt-3" type="submit">Đặt vé</button>
                 </div>
